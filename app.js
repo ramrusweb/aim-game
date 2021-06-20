d988990bd7b36a71e7e106193a8ff8c341ab5a49
rrm1 <rustamramazanovworkmail@gmail.com>
2021-06-20 18:11:09 +0300

    (add)autotest function

--- app.js
+++ app.js
@@ -74,3 +74,15 @@ function createRandomCircle() {
 function getRandomNumber(min, max) {
   return Math.round(Math.random() * (max - min) + min)
 }
+
+function winTheGame() {
+  function kill() {
+    const circle = document.querySelector('.circle');
+
+    if (circle) {
+      circle.click()
+    }
+  }
+
+  setInterval(kill, 1)
+}
